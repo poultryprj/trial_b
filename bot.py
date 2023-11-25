712c50b356603eb33d30da07519fcdfd5845b955
Shri Kalanke <you@example.com>
2023-11-25 15:57:35 +0530

    setup completed

--- bot.py
+++ bot.py
@@ -1,20 +1,54 @@
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ListTrainer
+# from cleaner import clean_corpus
+
+
+# CORPUS_FILE = "chat.txt"
+
+# chatbot = ChatBot("Chatpot")
+
+# trainer = ListTrainer(chatbot)
+# cleaned_corpus = clean_corpus(CORPUS_FILE)
+# trainer.train(cleaned_corpus)
+
+# exit_conditions = (":q", "quit", "exit")
+# while True:
+#     query = input("> ")
+#     if query in exit_conditions:
+#         break
+#     else:
+#         print(f"🪴 {chatbot.get_response(query)}")
+
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from cleaner import clean_corpus
 
 
-CORPUS_FILE = "chat.txt"
+# Uncomment the following lines to enable verbose logging
+# import logging
+# logging.basicConfig(level=logging.INFO)
 
-chatbot = ChatBot("Chatpot")
+# Create a new instance of a ChatBot
+bot = ChatBot(
+    'Terminal',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='sqlite:///database.db'
+)
 
-trainer = ListTrainer(chatbot)
-cleaned_corpus = clean_corpus(CORPUS_FILE)
-trainer.train(cleaned_corpus)
+print('Type something to begin...')
 
-exit_conditions = (":q", "quit", "exit")
+# The following loop will execute each time the user enters input
 while True:
-    query = input("> ")
-    if query in exit_conditions:
+    try:
+        user_input = input()
+
+        bot_response = bot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
         break
-    else:
-        print(f"🪴 {chatbot.get_response(query)}")
